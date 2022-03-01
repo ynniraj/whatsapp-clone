10851ee727ad9f41c5e7eaa82433377fc6817e71
Neeraj Yadav <yneeraj9111@gmail.com>
2022-03-01 19:13:57 +0530

    Changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHbwZ4bAv7M21xLpOgRnm1rmRucLABv1M",
-  authDomain: "whatsapp-ee719.firebaseapp.com",
-  projectId: "whatsapp-ee719",
-  storageBucket: "whatsapp-ee719.appspot.com",
-  messagingSenderId: "418659059565",
-  appId: "1:418659059565:web:767c6890eb95232d10fc64",
-  measurementId: "G-SDCVW0WERP"
+  apiKey: "AIzaSyCthgEBp_pQBcEk02PGjR-4PviChey9W5U",
+  authDomain: "whatsapp-clone-b7c63.firebaseapp.com",
+  projectId: "whatsapp-clone-b7c63",
+  storageBucket: "whatsapp-clone-b7c63.appspot.com",
+  messagingSenderId: "991605344354",
+  appId: "1:991605344354:web:5bbc517f63262ee240693b",
+  measurementId: "G-S6E2G6NFQN"
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore();
 const auth = firebase.auth()
